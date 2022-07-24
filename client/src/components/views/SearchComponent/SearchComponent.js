328e551ee5567e00e5a4edce0652ee47c270662f
namesace0427 <jaws02144@naver.com>
2022-07-24 21:34:59 +0900

    [DAY6-65] feat(searchbar): add address select box

--- client/src/components/views/SearchComponent/SearchComponent.js
+++ client/src/components/views/SearchComponent/SearchComponent.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  bubjungdong_sido,
+  bubjungdong_sigungu,
+  bubjungdong_dong,
+} from "./bubjungdong";
 
 const SearchComponent = () => {
-  return <div className="SearchComponent"></div>;
+  const [sido, setSido] = useState();
+  const [sigungu, setSigungu] = useState();
+  const [dong, setDong] = useState();
+
+  const handleSido = (e) => {
+    setSido(e.target.value);
+  };
+  const handleSigungu = (e) => {
+    setSigungu(e.target.value);
+  };
+  return (
+    <div className="SearchComponent">
+      <select id="sido" onChange={handleSido}>
+        <option value="">선택</option>
+        {bubjungdong_sido.map((data, idx) => {
+          return <option value={data.sido}>{data.codeNM}</option>;
+        })}
+      </select>
+      <select id="sigugun" onChange={handleSigungu}>
+        <option value="">선택</option>
+        {bubjungdong_sigungu.map((data, idx) => {
+          if (data.sido == sido)
+            return <option value={data.sigungu}>{data.codeNM}</option>;
+        })}
+      </select>
+      <select id="dong">
+        <option value="">선택</option>
+        {bubjungdong_dong.map((data, idx) => {
+          if (data.sido == sido && data.sigungu == sigungu)
+            return <option value={data.dong}>{data.codeNM}</option>;
+        })}
+      </select>
+    </div>
+  );
 };
 
 export default SearchComponent;
