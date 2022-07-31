e0f96f74455ee8caf8aa507733b43b199e28652f
delphox60 <delphox60@gmail.com>
2022-07-31 09:01:07 +0900

    feat(repository): search places by address code

--- server/fullbang/src/main/java/day6/fullbang/repository/PlaceRepository.java
+++ server/fullbang/src/main/java/day6/fullbang/repository/PlaceRepository.java
@@ -1,5 +1,7 @@
 package day6.fullbang.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
@@ -15,5 +17,13 @@ public class PlaceRepository {
 
     public Place findOne(Long id) {
         return em.find(Place.class, id);
+    }
+
+    public List<Place> findByAddressCode(String addressCodeHead) {
+        String addressCodeRegex = addressCodeHead + '%';
+        return em.createQuery("select p from Place p where p.address.addressCode like :addressCodeRegex",
+            Place.class)
+            .setParameter("addressCodeRegex", addressCodeRegex)
+            .getResultList();
     }
 }
