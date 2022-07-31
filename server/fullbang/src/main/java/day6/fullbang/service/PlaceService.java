1de795de51fa5f4c145cfd36effa752286f141ed
delphox60 <delphox60@gmail.com>
2022-07-31 09:13:01 +0900

    feat(service): search places by address code

--- server/fullbang/src/main/java/day6/fullbang/service/PlaceService.java
+++ server/fullbang/src/main/java/day6/fullbang/service/PlaceService.java
@@ -1,2 +1,22 @@
-package day6.fullbang.service;public class PlaceService {
+package day6.fullbang.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import day6.fullbang.domain.Place;
+import day6.fullbang.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class PlaceService {
+
+    private final PlaceRepository placeRepository;
+
+    public List<Place> findByAddressCode(String addressCodeHead) {
+        return placeRepository.findByAddressCode(addressCodeHead);
+    }
 }
